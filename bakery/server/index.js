df5f3ecde70f3ec6ca34c072d310792989bf60c0
Daniil Sergeev <daniil.scorpio@gmail.com>
2023-12-22 18:39:38 +0300

    feat: add bd config and sequelize code

--- bakery/server/index.js
+++ bakery/server/index.js
@@ -1,7 +1,20 @@
-import express from 'express';
-import mongoose from 'mongoose'; 
-import multer from 'multer';
-import cors from 'cors';
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./models/index");
 
-
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 const app = express();
+
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync({ alter: true });
+        app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();
